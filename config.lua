d088609ff6368b20b1d644a8c3a39dedd75ead08
p00ria <amir1300sobatdar@gmail.com>
2016-11-02 17:02:13 +0330

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,9 @@
+bot_token = "Your Token Here"
+send_api = "https://api.telegram.org/bot"..bot_token
+bot_version = "1.0"
+sudo_name = "Pouria"
+sudo_id = 142141024
+admingp = -135847900
+sudo_num = "639080373699"
+sudo_user = "This_Is_Pouria"
+sudo_ch = "CyberCH"
